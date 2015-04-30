d66bb1d34a06d7766016526f3b4251ab409c27a0
Michael Patalas <michael.patalas@paluno.uni-due.de>
2015-04-30 13:40:09 +0200

    Latest python-scripts

--- python/jsonToAuthorCsv.py
+++ python/jsonToAuthorCsv.py
@@ -1,10 +1,20 @@
 import json
 import codecs
 import sys
+import datetime
 from pprint import pprint
 from StringIO import StringIO
 
+def isMergeMessage(messageText):
+    if "successfully merged into" not in messageText:
+        if "successfully cherry" not in messageText:
+            if "successfully pushed" not in messageText:
+                return False
+    return True
+
+
 unknown = "unknown"
+num_lines = 0
 num_lines = sum(1 for line in open(sys.argv[1]))
 current_line_number = 0
 
@@ -32,18 +42,45 @@ with codecs.open(sys.argv[2], "w", "utf-8") as f:
                     if number > maxNumber:
                         maxNumber = number
                         currentRevisionId = revision
-                    
+           
+            if not jsondata.has_key("messages"):
+                sys.stderr.write("No messages in line " + str(current_line_number) + "\n")
+                continue;
+           
+            messages = jsondata["messages"]
             
+            if len(messages) == 0:
+                sys.stderr.write("0 messages in line " + str(current_line_number) + "\n")
+                continue;  
+            
+            newestMessage = ""
+            newestMessage = messages[0]
+            
+            for message in messages:
+                text = message["message"]
+                if not isMergeMessage(text):
+                    continue
+                
+                newestDate = datetime.datetime.strptime(newestMessage["date"], "%Y-%m-%d %H:%M:%S.%f000")
+                currentMessageDate = datetime.datetime.strptime(message["date"], "%Y-%m-%d %H:%M:%S.%f000")
+                if currentMessageDate > newestDate:
+                    newestMessage = message
+                        
+            
+            date = newestMessage["date"]
+            
+            if not isMergeMessage(newestMessage["message"]):
+                sys.stderr.write("No successfully merged into in line " + str(current_line_number) + "\n")
             
            
             currentRevision = revisions[currentRevisionId]
             
             if not currentRevision.has_key("commit"):
-                sys.stderr.write("No commit in line " + str(current_line_number))
+                sys.stderr.write("No commit in line " + str(current_line_number) + "\n")
                 continue
             
             authorEmail = currentRevision["commit"]["author"]["email"]
-            date = currentRevision["commit"]["committer"]["date"]
+            
             
 
             files = currentRevision["files"]
@@ -52,6 +89,7 @@ with codecs.open(sys.argv[2], "w", "utf-8") as f:
             
             for file in files:
                 filename = "" + file
+                filename = filename.replace(":", "_")
                 linesInserted = 0
                 if files[file].has_key("lines_inserted"): linesInserted = files[file]["lines_inserted"]
                 
@@ -77,4 +115,3 @@ with codecs.open(sys.argv[2], "w", "utf-8") as f:
                 resultLine += concatChar + unicode(resultItem)
                 concatChar = ";"
             f.write(resultLine + "\n")
-        
