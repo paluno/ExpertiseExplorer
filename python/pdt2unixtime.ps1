1a0c3d0768780183c555014e81640d72ed7e7229
bb-froggy <ch-git@hannebauer.name>
2015-05-04 17:41:45 +0200

    A small tool for time calculations

--- python/pdt2unixtime.ps1
+++ python/pdt2unixtime.ps1
@@ -1,4 +1,4 @@
-﻿$pdtDateString = "28.04.2009 16:33:15"
+﻿$pdtDateString = "12.11.2012 21:07:59"
 
 $pdtDate = [DateTime]::Parse($pdtDateString).AddHours(7)
 $startOfTime = new-object DateTime(1970,1,1,0,0,0,[DateTimeKind]::Utc)
