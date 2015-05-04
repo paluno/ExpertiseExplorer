6cd6fe0f721ca43c1b05962188b41fc938ab6f65
bb-froggy <ch-git@hannebauer.name>
2015-05-04 16:16:38 +0200

    Shell script to compute Unix times

---
+++ python/pdt2unixtime.ps1
@@ -0,0 +1,7 @@
+﻿$pdtDateString = "28.04.2009 16:33:15"
+
+$pdtDate = [DateTime]::Parse($pdtDateString).AddHours(7)
+$startOfTime = new-object DateTime(1970,1,1,0,0,0,[DateTimeKind]::Utc)
+$unixTime = $pdtDate.Subtract($startOfTime).TotalSeconds
+
+$unixTime
