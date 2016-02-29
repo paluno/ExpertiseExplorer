cf1b84b66a41cbe31590555a17f6ac0c774d072d
bb-froggy <ch-git@hannebauer.name>
2016-02-29 17:44:50 +0100

    classes for communication with GitLab

---
+++ ReviewerRecommender/Models/GitLabIncarnations/MergeRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ReviewerRecommender.Models.GitLab
+{
+    public partial class MergeRequest
+    {
+        public HttpClient GitLabAPI { get; set; }
+
+        public MergeRequest()
+        {
+            GitLabAPI = new HttpClient();
+            GitLabAPI.BaseAddress = new Uri("http://192.168.56.101/api/v3/");
+            GitLabAPI.DefaultRequestHeaders.Add("PRIVATE-TOKEN", "9koXpg98eAheJpvBs5tK");
+        }
+
+        public async Task<List<Commit>> FetchCommitsAsync()
+        {
+             HttpResponseMessage response = await GitLabAPI.GetAsync(
+                string.Format("/projects/{0}/merge_requests/{1}/commits", ProjectId, Id)
+                );
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsAsync<List<Commit>>();
+        }
+    }
+}
---
+++ ReviewerRecommender/Models/ProjectRecommender.cs
@@ -0,0 +1,57 @@
+﻿using ExpertiseExplorer.Algorithms;
+using ReviewerRecommender.Models.GitLab;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ReviewerRecommender.Models
+{
+    public class ProjectRecommender
+    {
+        public string ProjectName { get; }
+
+        //        public WeightedReviewCountAlgorithm recommendationAlgorithm { get; set; }
+
+        private static ConcurrentDictionary<string, ProjectRecommender> dictRecommenders { get; } 
+            = new ConcurrentDictionary<string, ProjectRecommender>();
+
+
+        public static ProjectRecommender FindProjectRecommender(string projectName)
+        {
+            if (dictRecommenders.ContainsKey(projectName))
+                return dictRecommenders[projectName];
+            else
+                lock(dictRecommenders)
+                    if (dictRecommenders.ContainsKey(projectName))
+                        return dictRecommenders[projectName];
+                    else
+                        return dictRecommenders[projectName] = new ProjectRecommender(projectName);
+        }
+
+        private ProjectRecommender(string projectName)
+        {
+            this.ProjectName = projectName;
+        }
+
+        public void orderReviewerRecommendation(MergeRequest mr)
+        {
+            ThreadPool.QueueUserWorkItem(delegate(object dummy) { recommendReviewersAsync(mr).Wait(); });
+        }
+
+        protected async Task recommendReviewersAsync(MergeRequest mr)
+        {
+            List<Commit> commits = await mr.FetchCommitsAsync();
+            IEnumerable<string> allFiles =
+                commits
+                    .Select(commit => commit.Added.Concat(commit.Modified).Concat(commit.Removed))
+                    .Aggregate((c1, c2) => c1.Union(c2));
+
+            // Todo: Calculate good reviewers, post to MR
+            //Algorithm.GetDevelopersForArtifactsAsync()
+        }
+    }
+}
